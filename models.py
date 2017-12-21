bb53fb3e797eb1d67d314928bc4341229bdf5603
Naren Dasan <naren@narendasan.com>
2017-12-20 18:45:03 -0600

    nvdocker name space, model controls db exculsively, user support

--- models.py
+++ models.py
@@ -1,15 +1,24 @@
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-db = SQLAlchemy()
+Base = declarative_base()
 
-class Ports(db.Model):
-    __tablename__ = 'docker_ports'
-    id = db.Column(db.Integer, primary_key=True)
-    port = db.Column(db.Integer)
-    link = db.Column(db.String(100))
+class InstanceAssigment(Base):
+    __tablename__ = 'InstanceAssignments'
+    id = Column(Integer, primary_key=True)
+    jupyter_port = Column(Integer)
+    monitor_port = Column(Integer)
+    jupyter_link = Column(String(100))
+    monitory_link = Column(String(100))
+    user = Column(String(10))
     
-    def __init__(self, port, link):
-        self.port = port
-        self.link = link
+    def __init__(self, jport, mport, jupyter_link, monitor_link, user):
+        self.jupyter_port = jport
+        self.monitor_port = mport
+        self.jupyter_link = jupyter_link
+        self.monitory_link = monitor_link
+        self.user = user
 
+    def __repr__(self):
+        return '<Assignment %r>' % (self.user)
