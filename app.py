9b690f9cef18640c9aa405092aa4f4c0e81ebb7f
Naren Dasan <naren@narendasan.com>
2018-02-25 01:57:21 -0600

    Unify (#11)
    * potential unification of frontend and backend
    
    * working merge of applications
    
    * suppress debugging
    
    * update driver version
    
    * support other formats

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, send_from_directory
 from flask_cors import CORS
 import logging
 import nvdocker
 from flask_sqlalchemy import SQLAlchemy
 from models import InstanceAssigment, Base
+import os
 import random
+import logging
 
-app = Flask(__name__)
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+app = Flask(__name__, static_folder='frontend/build', static_url_path='')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////opt/gpu_cluster/gpu_cluster_instances.db'
 
 instance_store = SQLAlchemy(app)
 CORS(app)
-PORT=5656
+PORT=4000
 docker_client = nvdocker.NVDockerClient()
 
 @app.before_first_request
@@ -21,27 +26,44 @@ def setup():
     Base.metadata.drop_all(bind=instance_store.engine)
     Base.metadata.create_all(bind=instance_store.engine)
 
+@app.route('/', defaults={'path': ''}, methods=['GET'])
+@app.route('/<path:path>')
+def serve_ui(path):
+    return send_from_directory(os.path.dirname(os.path.realpath(__file__)) + "/frontend/build", 'index.html') 
+
 @app.route('/create_container', methods=['POST'])
 def create_container():
     if not request.json or 'image' not in request.json:
         abort(400)
 
-    jport = get_port()
+    uport = get_port()
     mport = get_port()
-    while jport == mport:
+    while uport == mport:
         mport = get_port()
 
     user = ""    
     if 'user' in request.json:
         user = request.json['user']
 
-    c_id = docker_client.create_container('', image = request.json['image'], is_gpu = True, ports = (jport,mport), user = user)
-    token = docker_client.run_cmd(c_id, 'python3 /opt/cluster-container/jupyter_get.py')
-    jurl = "http://vault.acm.illinois.edu:{}/?token={}".format(jport, token.decode("utf-8") )
-    murl = "http://vault.acm.illinois.edu:" + str(mport)
-    instance_store.session.add(InstanceAssigment(jport, mport, jurl, murl, user))   
+    token_needed = False
+    if 'token_required' in request.json:
+        token_needed = bool(request.json['token_required'])
+
+    c_id = docker_client.create_container('', image = request.json['image'], is_gpu = True, ports = (uport,mport), user = user)
+    
+    uurl = ""
+    murl = ""
+    if token_needed: 
+        token = docker_client.run_cmd(c_id, 'python3 /opt/cluster-container/jupyter_get.py')
+        uurl = "http://vault.acm.illinois.edu:{}/?token={}".format(uport, token.decode("utf-8") )
+        murl = "http://vault.acm.illinois.edu:" + str(mport)
+    else:
+        uurl = "http://vault.acm.illinois.edu:" + str(uport)
+        murl = "http://vault.acm.illinois.edu:" + str(mport)
+    
+    instance_store.session.add(InstanceAssigment(uport, mport, uurl, murl, user))   
     instance_store.session.commit()
-    return jsonify({'jupyter_url' : jurl, 'monitor_url': murl})
+    return jsonify({'ui_url' : uurl, 'monitor_url': murl})
 
 def get_port():
     while True:
