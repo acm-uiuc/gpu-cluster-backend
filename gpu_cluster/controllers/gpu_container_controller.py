2ae70cf98441b3184aec212bfd701800a2008d59
jeffreyzhang <jeffrey.zh4ng@gmail.com>
2019-02-24 03:54:22 -0600

    Added self to some broken code

--- gpu_cluster/controllers/gpu_container_controller.py
+++ gpu_cluster/controllers/gpu_container_controller.py
@@ -10,7 +10,7 @@ class GPUContainerController(ContainerController):
         super().__init__(config)
         self.docker_client = NVDockerClient()
 
-    def create_container(image, user="", token_required=False, budget=-1, num_gpus=1):
+    def create_container(self, image, user="", token_required=False, budget=-1, num_gpus=1):
         # Get 2 open ports for UI and Monitor
         uport = super().get_port()
         mport = super().get_port()
@@ -40,33 +40,33 @@ class GPUContainerController(ContainerController):
         }
 
         # create container
-        container_list = docker_client.docker_image_list(filters={'name': image})
+        container_list = self.docker_client.docker_image_list(filters={'name': image})
         if container_list:
-            c_id = docker_client.run(image, '', container_config).id
+            c_id = self.docker_client.run(image, '', container_config).id
 
         else:
             # Add a client.images.search to check if the path to the container exists on docker hub. If not, error out
-            has_result = docker_client.docker_image_search(image)
+            has_result = self.docker_client.docker_image_search(image)
             if not has_result:
                 print('No image in DockerHub')
                 return 'No image in DockerHub' , '', ''
 
             image_tag = image.split(':')
-            docker_image = docker_client.docker_image_pull(image_tag[0], image_tag[1])
+            docker_image = self.docker_client.docker_image_pull(image_tag[0], image_tag[1])
 
             # If pull returns more than one image, get the first one in the list
             if hasattr(docker_image, '__len__'):
                 docker_image = docker_image[0]
 
             # Do you have to build the image after you pull it from Docker Hub?
-            c_id = docker_client.create_container(docker_image, '', container_config).id
+            c_id = self.docker_client.create_container(docker_image, '', container_config).id
 
         # assemble endpoints for UI, monitor and get the access token if needed
         uurl = ""
         murl = ""
         token = ""
         if token_required:
-            token = docker_client.exec_run(c_id, 'python3 /opt/cluster-container/jupyter_get.py')
+            token = self.docker_client.exec_run(c_id, 'python3 /opt/cluster-container/jupyter_get.py')
             uurl = "http://vault.acm.illinois.edu:{}/?token={}".format(uport, token.decode("utf-8"))
             murl = "http://vault.acm.illinois.edu:" + str(mport)
         else:
